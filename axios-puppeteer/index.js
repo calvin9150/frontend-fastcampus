46eca486fcf7fb661f09c23dd53465d27e5b3f87
calvin9150 <wonbae9150@gmail.com>
2021-05-21 18:16:52 +0900

    Get data from the page

--- axios-puppeteer/index.js
+++ axios-puppeteer/index.js
@@ -1,11 +1,23 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-axios.get("http://example.com").then((response) => {
-  const htmlString = response.data;
-  const $ = cheerio.load(htmlString);
-  const h1 = $("h1").text();
-  const href = $("a").text();
-  console.log(h1);
-  console.log(href);
-});
+const article = {};
+const crwaler = (pageNum) => {
+  axios
+    .get(
+      `https://api.brunch.co.kr/v1/search/article?q=%ED%92%80%EC%8A%A4%ED%83%9D&page=${pageNum}&pageSize=20&highlighter=y&escape=y&sortBy=accu`
+    )
+    .then((response) => {
+      const data = response.data;
+      article[pageNum] = data.data.list;
+      const nextNumber = pageNum + 1;
+      console.log("현재 페이지 : ", pageNum);
+      if (nextNumber < 10) {
+        crwaler(nextNumber);
+        return;
+      }
+      console.log(article);
+    });
+};
+
+crwaler(1);
