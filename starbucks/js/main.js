386f495e9ce0bc40652f7ffafeedb53883654668
calvin9150 <wonbae9150@gmail.com>
2021-06-15 19:56:58 +0900

    Add scroll effected badges  with lodash, gsap

--- starbucks/js/main.js
+++ starbucks/js/main.js
@@ -14,3 +14,17 @@ searchInputEl.addEventListener("blur", () => {
   searchEl.classList.remove("focused");
   searchInputEl.removeAttribute("placeholder");
 });
+
+const badgeEl = document.querySelector("header .badges");
+
+window.addEventListener(
+  "scroll",
+  _.throttle(() => {
+    console.log(window.scrollY);
+    if (window.scrollY > 500) {
+      gsap.to(badgeEl, 0.5, { opacity: 0, display: "none" });
+    } else {
+      gsap.to(badgeEl, 0.5, { opacity: 1, display: "block" });
+    }
+  }, 300)
+);
